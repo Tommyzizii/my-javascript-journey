37047c5d57a89816ebdfe4507afb22f7b187df2a
Tommy <thantzinmin.us@gmail.com>
2025-10-13 14:29:04 +0700

    learning: data types

--- Fundamentals-Part-1/script.js
+++ Fundamentals-Part-1/script.js
@@ -23,3 +23,25 @@ Use Underscore or $ to keep a variable name of reserved keyword
 Variable name should be descriptive
 
 */
+
+let tommyIsStudent = true;
+console.log(tommyIsStudent);
+// --- There are only two data types in JS, which are primitive and object. ---
+
+console.log(typeof tommyIsStudent);
+console.log(typeof 32);
+// --- Use typeof to see what is the type of this variable. ---
+
+let age;
+// ---  undefine variable ---
+console.log(typeof age);
+
+age = 21;
+console.log(age);
+// --- JS allows dynamic typing. ---
+
+let year = null;
+console.log(typeof year);
+// --- object is type of null. This is also an error. ---
+
+
