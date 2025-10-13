7f8868a65e6bb9dc94c6af5224089f380c397c1e
Tommy <thantzinmin.us@gmail.com>
2025-10-13 13:21:01 +0700

    learning: values and variables

--- Fundamentals-Part-1/script.js
+++ Fundamentals-Part-1/script.js
@@ -1,3 +1,25 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
+// if (js === "amazing") alert("JavaScript is FUN!");
 console.log(40 + 8 + 23 - 10);
+
+console.log('Tommy');
+
+let firstName = "Tommy";
+console.log(firstName);
+
+let secondName = "ZiZii";
+console.log(secondName);
+
+/*
+!!!! Syntax error; !!!!
+
+let 2years = 2;
+let tommy$zizii = tz;
+let new = 27; <- new is reserved keyword in JS
+let function = 27 <- function is also reserved keyword in JS
+
+-- Optimize Way -- 
+Use Underscore or $ to keep a variable name of reserved keyword
+Variable name should be descriptive
+
+*/
