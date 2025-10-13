c7c418f3d17da0d52acf0ec608f7ddf20caacc03
Tommy <thantzinmin.us@gmail.com>
2025-10-13 15:21:23 +0700

    learning: basic operators

---
+++ Fundamentals-Part-1/04-basic-operators.js
@@ -0,0 +1,34 @@
+// --- Mathematic Operators ---
+const currentYear = 2025;
+const tommyAge = currentYear - 2004;
+console.log(tommyAge);
+// --- Calculating Discounts ---
+const discount = 10;
+const normalPrice = 1499;
+const discountPrice = 10 / 100 * 1499;
+const finalPrice = normalPrice - discountPrice
+console.log(finalPrice);
+
+// --- Assignments Operators ---
+let x  = 10 + 5;
+console.log(x);
+
+x += 10;
+console.log(x);
+
+x *= 4; 
+console.log(x);
+
+x++;
+console.log(x);
+
+x--;
+console.log(x);
+
+// --- Comparison Operators ---
+const johnAge = currentYear - 2004;
+const henryAge = currentYear - 2007;
+
+console.log(johnAge > 18);
+console.log(henryAge < 18);
+console.log(henryAge <= 18);
