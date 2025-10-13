4a655da59a69ccd127670324ebb7a7615c6c2535
Tommy <thantzinmin.us@gmail.com>
2025-10-13 13:24:47 +0700

    practice: assignment added

---
+++ Fundamentals-Part-1/assignment.js
@@ -0,0 +1,14 @@
+/*
+Values and Variables﻿
+
+Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+Log their values to the console.
+*/
+
+country = 'Myanmar';
+continent = 'Asia';
+population = 50;
+
+console.log(country);
+console.log(continent);
+console.log(population);
