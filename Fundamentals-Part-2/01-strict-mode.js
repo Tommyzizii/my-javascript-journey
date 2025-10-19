dfe1e1972fa00f9064a40c4d4e133ed916c2e936
Tommy <thantzinmin.us@gmail.com>
2025-10-19 20:12:37 +0700

    learning: strict mode

---
+++ Fundamentals-Part-2/01-strict-mode.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let hasDriversLiscense = false;
+const passTest = true;
+
+if (passTest) hasDriverLiscense = true; // <- Strict mode will indicate to this line
+if (hasDriversLiscense) console.log('I can drive :D');
+
+const interface = 'Audio'; // <- Unexpected strict mode reserved wordUnderstand this error
+
