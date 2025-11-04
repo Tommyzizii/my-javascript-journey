00294b07fc386e2502dc42f78fb1b956d2034ec0
Tommy <thantzinmin.us@gmail.com>
2025-11-04 21:48:32 +0700

    learning: objects

---
+++ Fundamentals-Part-2/08-objects.js
@@ -0,0 +1,17 @@
+const TommyData = [
+    'Tommy',
+    'Zizii',
+    2025-2004,
+    'Student',
+    ['No one']
+];
+
+const tommy = {
+    firstName: 'tommy',
+    lastName: 'Zizii',
+    age: 2025 - 2004,
+    job: 'Student',
+    friends: ['No one']
+}
+
+console.log(tommy.age);
