0561289bf18503a387ae2289d6a6d4fb08096ede
Tommy <thantzinmin.us@gmail.com>
2025-11-05 10:57:40 +0700

    learning: loop backwards and loop in loop

---
+++ Fundamentals-Part-2/13-loop-backwards-&-loop-in-loop.js
@@ -0,0 +1,18 @@
+const tommy = [
+    'Tommy',
+    'Zizii',
+    2025-2004,
+    'Student',
+    ['No one']
+
+];
+
+for(let i = tommy.length - 1; i >= 0; i--){
+    console.log(i, tommy[i]);
+}
+
+for(let i = 1; i <= 3; i++){
+    for(let y = 0; y <= 12; y++){
+        console.log(`Doing Exercise ${i}, and this is ${y} reps`);
+    }
+}
