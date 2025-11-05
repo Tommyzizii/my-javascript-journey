bb59afd26250e3b7b65ecf1b72abd56d18e069a4
Tommy <thantzinmin.us@gmail.com>
2025-11-05 08:54:57 +0700

    learning: iterations with for and while loop

---
+++ Fundamentals-Part-2/11-iteration.js
@@ -0,0 +1,12 @@
+// For Loop
+
+for (let reps = 1; reps <= 10; reps++){
+    console.log(`Lifiing weights repetition ${reps} 🏋🏻‍♀️`);
+}
+
+// While Loop
+let reps = 11;
+while (reps <= 20){
+    console.log(`Lifiing weights repetition ${reps} 🏋🏻‍♀️`);
+    reps++;
+}
