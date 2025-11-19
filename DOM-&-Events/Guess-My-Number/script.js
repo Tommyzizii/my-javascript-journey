bafefefe919292fb27186aebe77a43f7186211ba
Tommy <thantzinmin.us@gmail.com>
2025-11-19 13:10:00 +0700

    lecture: learning some DOM Manipulations for Number Guessing game

--- DOM-&-Events/Guess-My-Number/script.js
+++ DOM-&-Events/Guess-My-Number/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+
+// Making .message to "Correct Number"
+document.querySelector('.message').textContent = '🎉Correct Number!!!';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.score').textContent = 30;
+console.log(document.querySelector('.score').textContent);
+
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
