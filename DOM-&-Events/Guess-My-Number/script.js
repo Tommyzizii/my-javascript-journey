1f74b7e209d3f91d7e7ed57383a3d9f32224bd58
Tommy <thantzinmin.us@gmail.com>
2025-11-23 22:46:21 +0700

    learning: add some event handler along with dom manipulation

--- DOM-&-Events/Guess-My-Number/script.js
+++ DOM-&-Events/Guess-My-Number/script.js
@@ -1,14 +1,36 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
-// Making .message to "Correct Number"
-document.querySelector('.message').textContent = '🎉Correct Number!!!';
-console.log(document.querySelector('.message').textContent);
+// // Making .message to "Correct Number"
+// document.querySelector('.message').textContent = '🎉Correct Number!!!';
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.score').textContent = 30;
-console.log(document.querySelector('.score').textContent);
+// document.querySelector('.score').textContent = 30;
+// console.log(document.querySelector('.score').textContent);
 
-console.log(document.querySelector('.guess').value);
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+// console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+let guess;
+const secretNumber = Math.trunc(Math.random()*100);
+console.log(secretNumber);
+
+document.querySelector('.check').addEventListener
+('click', function(){
+    guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    if(!guess){
+        document.querySelector('.message').textContent = "Not a number!🤚🏻"
+    }
+});
+
+document.querySelector('.again').addEventListener
+('click', function(){
+    if(!guess){
+        document.querySelector('.message').textContent = "Write a number or use ⬆️/⬇️ to select a number"
+
+    }
+})
