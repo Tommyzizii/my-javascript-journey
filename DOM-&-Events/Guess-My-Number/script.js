bc617b25b949c1baf3222b5b8e9019c0035c9fe1
Tommy <thantzinmin.us@gmail.com>
2025-12-02 21:13:45 +0700

    feat: set up for high score

--- DOM-&-Events/Guess-My-Number/script.js
+++ DOM-&-Events/Guess-My-Number/script.js
@@ -14,8 +14,8 @@
 // console.log(document.querySelector('.guess').value);
 
 let score = 20;
-
-const secretNumber = Math.trunc(Math.random() * 100) + 1;
+let highscore = 0;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 document.querySelector('.check').addEventListener('click', function () {
   let guess = Number(document.querySelector('.guess').value);
@@ -32,6 +32,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '🎉 Correct Number';
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if(score > highscore){
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     //guess is too high
   } else if (guess > secretNumber) {
@@ -57,7 +62,7 @@ document.querySelector('.check').addEventListener('click', function () {
 
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
-  secretNumber = Math.trunc(Math.random() * 100) + 1;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
 
   document.querySelector('.message').textContent = 'Start guessing...';
   document.querySelector('.score').textContent = score;
