ded62a905fe6623b803200093ab630b00bb84c38
Tommy <thantzinmin.us@gmail.com>
2025-12-08 22:51:02 +0700

    learning: TDZ

--- 03-JS-Behind-The-Scene/script.js
+++ 03-JS-Behind-The-Scene/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Scope chains practice
 function calcAge(birthYear){
     const age = 2025 - birthYear;
     
@@ -25,4 +26,41 @@ function calcAge(birthYear){
 const firstName = 'Tommy';
 calcAge(2004);
 
+// TDZ in practice
+// console.log(me);
+// console.log(job);
+// console.log(year);
 
+// var me = 'Tommy';
+// let job = 'student';
+// const year = 2004;
+
+// Functions in TDZ
+
+// console.log(addDec(2,3));
+// console.log(addExpr(2,3));
+// console.log(addArrow(2,3));
+
+// function addDec(a,b){
+//     return a + b;
+// } // can call before declaration
+
+// const addExpr = function (a,b){
+//     return a +b;
+// } 
+
+// const addArrow = (a,b) => a + b;
+
+// Example
+
+
+var numPro = 0;
+
+if(numPro <= 0){
+    deleteShoppingCart();
+}
+
+
+function deleteShoppingCart(){
+    console.log('All Deleted');
+}
