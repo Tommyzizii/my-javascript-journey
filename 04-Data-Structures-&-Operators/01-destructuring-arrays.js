5c617f216ea842cd195b8ccb72e532a61c582f20
Tommy <thantzinmin.us@gmail.com>
2025-12-14 15:42:23 +0700

    learning: destructuring array

---
+++ 04-Data-Structures-&-Operators/01-destructuring-arrays.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order: function(starterIndex, mainIndex){
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+  },
+};
+
+const arr = [2,3,4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[3];
+
+// Destructuring assignment
+const [x,y,z] = arr;
+console.log(x,y,z);
+
+console.log(arr);
+
+const [first, , , last] = restaurant.categories;
+console.log(first,last);
+
+let [main, ,secondary] = restaurant.categories;
+console.log(main,secondary);
+
+// Switching Array
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main,secondary);
+
+[main, secondary] = [secondary, main];
+console.log(main,secondary);
+
+console.log(restaurant.order(3, 2));
+
+// Recieve 2 return values from a functions
+const [starter, mainCourse] = restaurant.order(2,0);
+console.log(starter, mainCourse);
+
+// Nested Array
+const nested = [2,3,[5,6]];
+// const [i, ,j] = nested;
+// console.log(i,j)
+
+// Destructing array within the nested array
+const [i, , [j , k]] = nested;
+console.log(i, j, k);
+
+// Default Values
+const [p=1,q=1,r=1] = [8,9]
+console.log(p,q,r);
